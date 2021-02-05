398f8e20200fb6563deabb88eabe72e12509c9f0
John Stechschulte <john@picknik.ai>
2021-02-05 12:58:15 -0700

    reorg

--- docker/start.sh
+++ docker/start.sh
@@ -6,6 +6,7 @@ docker run -it --rm \
   --name cam-survey \
   --network host \
   --privileged \
+  --device=/dev/ttyUSB0 \
   -e DISPLAY=$DISPLAY \
   -v /tmp/.X11-unix:/tmp/.X11-unix \
   -v $HOME/.Xauthority:/root/.Xauthority \
